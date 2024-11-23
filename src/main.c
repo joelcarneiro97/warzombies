5f1524e149e656f233e186878f6d1078b8f1fcd8
Joel Carneiro <joelcarneiro97@gmail.com>
2024-11-23 05:06:07 +0000

    1st steps

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include "raylib.h"
+#include <stdio.h>
+
+int main()
+{
+    const int height = 720;
+    const int width = 1280;
+
+    InitWindow(width, height, "Warzombie");
+    SetTargetFPS(120);
+
+    scanf(NULL);
+}
+
